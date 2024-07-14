865e2e616cd7e75e90158039271f4866939645e4
primalkarma <115176571+primalkarma@users.noreply.github.com>
2024-07-14 19:41:07 +0000

    finished client app

--- crm/client/views.py
+++ crm/client/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from .models import Client
+from .forms import AddClientForm
 
 # Create your views here.
 
@@ -17,3 +19,50 @@ def clients_detail(request, pk):
     return render(request, 'client/clients_detail.html', {
         'client': client
     })
+
+@login_required
+def clients_add(request):
+    if request.method == 'POST':
+        form = AddClientForm(request.POST)
+
+        if form.is_valid:
+            client = form.save(commit=False)
+            client.created_by = request.user
+            client.save()
+
+            messages.success(request, "The client was sucessfully created!")
+
+            return redirect('clients_list')
+    else:
+        form = AddClientForm()
+
+    return render(request, 'client/clients_add.html', {
+        'form': form
+    })
+
+@login_required
+def clients_delete(request, pk):
+    client = get_object_or_404(Client, created_by=request.user, pk=pk)
+    client.delete()
+
+    messages.success(request, "The client was sucessfully deleted")
+
+    return redirect('clients_list')
+
+@login_required
+def clients_edit(request, pk):
+    client = get_object_or_404(Client, created_by=request.user, pk=pk)
+
+    if request.method == 'POST':
+        form = AddClientForm(request.POST, instance=client)
+
+        if form.is_valid:
+            form.save()
+            messages.success(request, "The client was sucessfully edited!")
+
+            return redirect('clients_list')
+    else:
+        form = AddClientForm(instance=client)
+        return render(request, 'client/clients_edit.html', {
+            'form': form
+        })
